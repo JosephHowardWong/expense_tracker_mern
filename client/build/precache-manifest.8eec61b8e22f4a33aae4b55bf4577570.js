904abb01fa72650037a9486e271ba46d61858056
Joseph Wong <josephwong@Josephs-MacBook-Pro.local>
2020-06-16 15:23:32 -0400

    Initial Commit

---
+++ client/build/precache-manifest.8eec61b8e22f4a33aae4b55bf4577570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67166c0cda2102c33bc51c1f4fad674e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00e573008834d6032859",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "03217c82b9b519777e78",
+    "url": "/static/js/2.e673f05c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e673f05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e573008834d6032859",
+    "url": "/static/js/main.767977c6.chunk.js"
+  },
+  {
+    "revision": "ded419595feb73f778e6",
+    "url": "/static/js/runtime-main.23acf746.js"
+  }
+]);
